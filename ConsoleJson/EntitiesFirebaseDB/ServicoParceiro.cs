1ac0da9e200fe7fe4db0995305f5223ecf42f82e
FlavineiOliveira <34008386+FlavineiOliveira@users.noreply.github.com>
2018-07-03 23:59:32 -0300

    Add files via upload

---
+++ ConsoleJson/EntitiesFirebaseDB/ServicoParceiro.cs
@@ -0,0 +1,41 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleJson.EntitiesFirebaseDB
+{
+	public class Servico
+	{
+		//[JsonProperty(PropertyName = "EnderecoClinica")]
+		public string EnderecoClinica { get; set; }
+
+		//[JsonProperty(PropertyName = "NomeClinica")]
+		public string NomeClinica { get; set; }
+
+		//[JsonProperty(PropertyName = "Orientacoes")]
+		public string Orientacoes { get; set; }
+
+		//[JsonProperty(PropertyName = "TipoCategoria")]
+		public string TipoCategoria { get; set; }
+
+		//[JsonProperty(PropertyName = "TipoSubCategoria")]
+		public string TipoSubCategoria { get; set; }
+
+		//[JsonProperty(PropertyName = "ValorServico")]
+		public int ValorServico { get; set; }
+	}
+
+	public class RootServicoParceiro
+	{
+		//[JsonProperty(PropertyName = "ContadorServicoId")]
+		public int ContadorServicoId { get; set; }
+
+		//[JsonProperty(PropertyName = "Servico")]
+		public List<Servico> Servico { get; set; }
+
+	}
+}
---
+++ ConsoleJson/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConsoleJson.EntitiesFirebaseDB;
+using Newtonsoft.Json;
+
+namespace ConsoleJson
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string result = "{ \"ContadorServicoId\":2,\"Servico1\":{ \"EnderecoClinica\":\"Avenida Guimarães\",\"NomeClinica\":\"Sorridents\",\"Orientacoes\":\"Ficar de jejum\",\"TipoCategoria\":\"Dentista\",\"TipoSubCategoria\":\"Cirurgião\",\"ValorServico\":100},\"Servico2\":{ \"EnderecoClinica\":\"Avenida Hum\",\"NomeClinica\":\"DenteClean\",\"Orientacoes\":\"Nenhuma orientação recomendada\",\"TipoCategoria\":\"Dentista\",\"TipoSubCategoria\":\"Cirurgião\",\"ValorServico\":90},}";
+
+			//Contador tem que receber o valor do ContadorServicoId do Json
+			int contadorId = 2;
+
+			result = result.Replace("\":{", "\":[{").Replace("},", "}],");
+
+			for (int id = 0; id <= contadorId; id++)
+			{
+				result = result.Replace("\"Servico" + id + "\":", "\"Servico\":");
+			}
+
+			//Apenas para acompanhamento de teste
+			//Console.WriteLine("Result com Replace -> " + result);
+
+			RootServicoParceiro teste = JsonConvert.DeserializeObject<RootServicoParceiro>(result);
+
+			/* Apenas para acompanhamento de teste
+			foreach (var item in teste.Servico)
+			{
+				Console.WriteLine("FOREACH == " + item.ValorServico.ToString());
+			}
+			*/
+
+			Console.WriteLine("Resultado == " + teste);
+			Console.ReadLine();
+		}
+
+	}
+}
